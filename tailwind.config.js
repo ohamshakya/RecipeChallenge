a8b8197b00a5e753d04c24fb1e231fa2054527bc
Oham Shakya <ohamshakya40@gmail.com>
2024-06-09 23:10:18 +0545

    tailwind commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors:{
+      "dark-raspberry" : "hsl(332, 51%, 32%)",
+      "White-clr" : "hsl(0, 0%, 100%)",
+      "Nutmeg" : "hsl(14, 45%, 36%)",
+      "Rose-White" : "hsl(330, 100%, 98%)",
+      "Light-grey" : "hsl(30, 18%, 87%)",
+      "Egg-shell" : " hsl(30, 54%, 90%)",
+      "Wenge-brown" : "hsl(30, 10%, 34%)",
+      "Dark-Charcoal" : "hsl(24, 5%, 18%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
